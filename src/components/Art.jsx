f48fb9476be11eb4420afe4913bcca55e6fa156d
fat-juicy-eggs <yiffany4life@gmail.com>
2024-08-22 13:24:23 -0400

    edited art

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Art = () => {
+    const images = [
+        './one.png',
+        './two.png',
+        './three.png',
+        './four.png',
+        './five.png'
+    ];
     return (
         <div className='lg:pb-20' id='Art'>
             <div className='font-bold text-xl md:text-2xl lg:text-3xl sticky top-0 w-full py-2 text-green-500 bg-white opacity-95'>
@@ -10,9 +19,24 @@ const Art = () => {
                 <h1>
                     3D art in Blender
                 </h1>
-                <p className='text-sm'>
-                    Placeholder for carousel
-                </p>
+                <div className='max-w-full mx-5'>
+                    <Carousel 
+                        showArrows={true}
+                        autoPlay={true}
+                        infiniteLoop={true}
+                        showThumbs={false}
+                        showStatus={false}
+                        interval={3000}
+                        stopOnHover={true}
+                        swipeable={true}
+                    >
+                        {images.map((image, index) => (
+                            <div key={index}>
+                                <img src={image} className='w-full h-auto object-cover rounded-lg'></img>
+                            </div>
+                        ))}
+                    </Carousel>
+                </div>
                 <h1>
                     Violin
                 </h1>
